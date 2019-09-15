22099c0ab4fdb1edcb1eb14f9c60081afa34db3b
Jonah Colagross <colagjon000@battlegroundps.org>
2019-09-15 13:13:23 -0700

    Prints M I S S I S S I P P I vertically.

--- src/Mississippi.java
+++ src/Mississippi.java
@@ -3,7 +3,65 @@ public class Mississippi {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		letterM();
+		iss();
+		iss();
+		letterI();
+		letterP();
+		letterP();
+		letterI();
+	}
+	public static void iss() {
+		letterI();
+		letterS();
+		letterS();
+	}
+	public static void letterM() {
+		letterMWide();
+		System.out.println("MM   MM");
+		System.out.println("M M M M");
+		System.out.println("M  M  M");
+		letterMWide();
+		letterMWide();
+		letterMWide();
+		System.out.println();
+	}
+		
+	public static void letterI() {
+		letterIEnds();
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		System.out.println("  I  ");
+		letterIEnds();
+		System.out.println();
+	}
+	public static void letterS() {
+		System.out.println(" SSSSS ");
+		System.out.println("S     S");
+		System.out.println("S      ");
+		System.out.println(" SSSSS ");
+		System.out.println("      S");
+		System.out.println("S     S");
+		System.out.println(" SSSSS ");
+		System.out.println();
+	}
+	public static void letterP() {
+		System.out.println("PPPPPP ");
+		System.out.println("P     P");
+		System.out.println("P     P");
+		System.out.println("PPPPPP ");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println("P");
+		System.out.println();
+	}
+	public static void letterMWide() {
+		System.out.println("M     M");
+	}
+	public static void letterIEnds() {
+		System.out.println("IIIII");
 	}
 
 }
